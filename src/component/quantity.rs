1ea59f01dd37a6e3f721dc3fd15df7d05d42e734
Nikita Galaiko <nikita@galaiko.rocks>
2024-10-02 15:50:33 +0200

    add decimal_quantity

--- src/component/quantity.rs
+++ src/component/quantity.rs
@@ -15,33 +15,21 @@ impl Quantity {
     }
 }
 
+// decimal_quantity is just like quantity, but requires decimal part.
+pub fn decimal_quantity() -> impl Parser<char, Quantity, Error = Simple<char>> {
+    let integer_trailing = digits().then_ignore(one_of(",.")).map(|integer| Quantity {
+        mantissa: integer.parse().unwrap(),
+        places: 0,
+    });
+
+    thousands_and_decimals().or(decimal()).or(integer_trailing)
+}
+
 pub fn quantity() -> impl Parser<char, Quantity, Error = Simple<char>> {
-    let digit = filter::<_, _, Simple<char>>(|c: &char| c.is_ascii_digit());
-    let one_to_three_digits = digit.repeated().at_least(1).at_most(3).collect::<String>();
-    let three_digits = digit.repeated().exactly(3).collect::<String>();
-    let none_or_more_digits = digit.repeated().collect::<String>();
-    let digits = digit.repeated().at_least(1).collect::<String>();
-
-    let fraction = |mark: char| just(mark).ignore_then(none_or_more_digits);
-    let decimal = |mark: char| digits.then(fraction(mark));
-    let thousands = |mark: char| {
-        one_to_three_digits.then(
-            three_digits
-                .separated_by(just(mark))
-                .allow_leading()
-                .at_least(1),
-        )
-    };
-
-    let thousands_and_decimals = thousands(',')
-        .then(fraction('.'))
-        .or(thousands('.').then(fraction(',')))
-        .map(|(thousands, decimals)| Quantity {
-            mantissa: (thousands.0 + thousands.1.join("").as_ref() + decimals.as_ref())
-                .parse()
-                .unwrap(),
-            places: decimals.len().try_into().unwrap(),
-        });
+    let fraction = fraction(',').or(fraction('.')).map(|fraction| Quantity {
+        mantissa: fraction.parse().unwrap(),
+        places: fraction.len().try_into().unwrap(),
+    });
     let thousands = thousands(',').or(thousands('.')).map(|(first, rest)| {
         if rest.len() == 1 {
             // i.e. "1,234" - it could be either a decimal, or an integer with a thousands
@@ -57,157 +45,311 @@ pub fn quantity() -> impl Parser<char, Quantity, Error = Simple<char>> {
             }
         }
     });
-    let decimal = decimal(',')
+    let digits = digits().map(|integer| Quantity {
+        mantissa: integer.parse().unwrap(),
+        places: 0,
+    });
+    thousands_and_decimals()
+        .or(thousands)
+        .or(decimal())
+        .or(fraction)
+        .or(digits)
+}
+
+fn digit() -> impl Parser<char, char, Error = Simple<char>> {
+    filter::<_, _, Simple<char>>(|c: &char| c.is_ascii_digit())
+}
+
+fn digits() -> impl Parser<char, String, Error = Simple<char>> {
+    digit().repeated().at_least(1).collect::<String>()
+}
+
+fn one_to_three_digits() -> impl Parser<char, String, Error = Simple<char>> {
+    digit()
+        .repeated()
+        .at_least(1)
+        .at_most(3)
+        .collect::<String>()
+}
+
+fn three_digits() -> impl Parser<char, String, Error = Simple<char>> {
+    digit().repeated().exactly(3).collect::<String>()
+}
+
+fn none_or_more_digits() -> impl Parser<char, String, Error = Simple<char>> {
+    digit().repeated().collect::<String>()
+}
+
+fn fraction(mark: char) -> impl Parser<char, String, Error = Simple<char>> {
+    just(mark).ignore_then(none_or_more_digits())
+}
+
+fn thousands_and_decimals() -> impl Parser<char, Quantity, Error = Simple<char>> {
+    thousands(',')
+        .then(fraction('.'))
+        .or(thousands('.').then(fraction(',')))
+        .map(|(thousands, decimals)| Quantity {
+            mantissa: (thousands.0 + thousands.1.join("").as_ref() + decimals.as_ref())
+                .parse()
+                .unwrap(),
+            places: decimals.len().try_into().unwrap(),
+        })
+}
+
+fn thousands(mark: char) -> impl Parser<char, (String, Vec<String>), Error = Simple<char>> {
+    one_to_three_digits().then(
+        three_digits()
+            .separated_by(just(mark))
+            .allow_leading()
+            .at_least(1),
+    )
+}
+
+fn decimal() -> impl Parser<char, Quantity, Error = Simple<char>> {
+    let decimal = |mark: char| digits().then(fraction(mark));
+    decimal(',')
         .or(decimal('.'))
         .map(|(integer, decimals)| Quantity {
             mantissa: (integer + decimals.as_ref()).parse().unwrap(),
             places: decimals.len().try_into().unwrap(),
-        });
-    let fraction = fraction(',').or(fraction('.')).map(|fraction| Quantity {
-        mantissa: fraction.parse().unwrap(),
-        places: fraction.len().try_into().unwrap(),
-    });
-    let digits = digits.map(|integer| Quantity {
-        mantissa: integer.parse().unwrap(),
-        places: 0,
-    });
-
-    thousands_and_decimals
-        .or(thousands)
-        .or(decimal)
-        .or(fraction)
-        .or(digits)
+        })
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn integer() {
-        let result = quantity().then_ignore(end()).parse("123");
-        assert!(result.is_ok());
-        assert_eq!(
-            result,
-            Ok(Quantity {
-                mantissa: 123,
-                places: 0,
-            })
-        );
+    mod quantity {
+        use super::*;
+
+        #[test]
+        fn integer() {
+            let result = quantity().then_ignore(end()).parse("123");
+            assert_eq!(
+                result,
+                Ok(Quantity {
+                    mantissa: 123,
+                    places: 0,
+                })
+            );
+        }
+
+        #[test]
+        fn integer_trailing() {
+            let result1 = quantity().then_ignore(end()).parse("123.");
+            let result2 = quantity().then_ignore(end()).parse("123,");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 123,
+                    places: 0,
+                })
+            );
+        }
+
+        #[test]
+        fn decimals_leading() {
+            let result1 = quantity().then_ignore(end()).parse(".0123");
+            let result2 = quantity().then_ignore(end()).parse(",0123");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 123,
+                    places: 4,
+                })
+            );
+        }
+
+        #[test]
+        fn decimals_invalid() {
+            let result = quantity().then_ignore(end()).parse("1..23");
+            assert!(result.is_err());
+        }
+
+        #[test]
+        fn decimals() {
+            let result1 = quantity().then_ignore(end()).parse("1.23");
+            let result2 = quantity().then_ignore(end()).parse("1,23");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 123,
+                    places: 2,
+                })
+            );
+        }
+
+        #[test]
+        fn decimals_like_thousands() {
+            let result1 = quantity().then_ignore(end()).parse("1.234");
+            let result2 = quantity().then_ignore(end()).parse("1,234");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 1234,
+                    places: 3,
+                })
+            );
+        }
+
+        #[test]
+        fn thousands_trailing() {
+            let result1 = quantity().then_ignore(end()).parse("12,345,678.");
+            let result2 = quantity().then_ignore(end()).parse("12.345.678,");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 12_345_678,
+                    places: 0,
+                })
+            );
+        }
+
+        #[test]
+        fn thousands_invalid() {
+            let result = quantity().then_ignore(end()).parse("12.34.678");
+            assert!(result.is_err());
+        }
+
+        #[test]
+        fn thousands() {
+            let result1 = quantity().then_ignore(end()).parse("12,345,678");
+            let result2 = quantity().then_ignore(end()).parse("12.345.678");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 12_345_678,
+                    places: 0,
+                })
+            );
+        }
+
+        #[test]
+        fn thousands_and_decimals() {
+            let result1 = quantity().then_ignore(end()).parse("12,345.678");
+            let result2 = quantity().then_ignore(end()).parse("12.345,678");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 12_345_678,
+                    places: 3,
+                })
+            );
+        }
     }
 
-    #[test]
-    fn integer_trailing() {
-        let result1 = quantity().then_ignore(end()).parse("123.");
-        let result2 = quantity().then_ignore(end()).parse("123,");
-        assert_eq!(result1, result2);
-        assert!(result2.is_ok());
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 123,
-                places: 0,
-            })
-        );
-    }
+    mod decimal_quantity {
+        use super::*;
 
-    #[test]
-    fn decimals_leading() {
-        let result1 = quantity().then_ignore(end()).parse(".0123");
-        let result2 = quantity().then_ignore(end()).parse(",0123");
-        assert_eq!(result1, result2);
-        assert!(result2.is_ok());
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 123,
-                places: 4,
-            })
-        );
-    }
+        #[test]
+        fn integer() {
+            let result = decimal_quantity().then_ignore(end()).parse("123");
+            assert!(result.is_err());
+        }
 
-    #[test]
-    fn decimals_invalid() {
-        let result = quantity().then_ignore(end()).parse("1..23");
-        assert!(result.is_err());
-    }
+        #[test]
+        fn integer_trailing() {
+            let result1 = decimal_quantity().then_ignore(end()).parse("123.");
+            let result2 = decimal_quantity().then_ignore(end()).parse("123,");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 123,
+                    places: 0,
+                })
+            );
+        }
 
-    #[test]
-    fn decimals() {
-        let result1 = quantity().then_ignore(end()).parse("1.23");
-        let result2 = quantity().then_ignore(end()).parse("1,23");
-        assert_eq!(result1, result2);
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 123,
-                places: 2,
-            })
-        );
-    }
+        #[test]
+        fn decimals_leading() {
+            let result1 = decimal_quantity().then_ignore(end()).parse(".0123");
+            let result2 = decimal_quantity().then_ignore(end()).parse(",0123");
+            assert!(result1.is_err());
+            assert!(result2.is_err());
+        }
 
-    #[test]
-    fn decimals_like_thousands() {
-        let result1 = quantity().then_ignore(end()).parse("1.234");
-        let result2 = quantity().then_ignore(end()).parse("1,234");
-        assert_eq!(result1, result2);
-        assert!(result2.is_ok());
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 1234,
-                places: 3,
-            })
-        );
-    }
+        #[test]
+        fn decimals_invalid() {
+            let result = decimal_quantity().then_ignore(end()).parse("1..23");
+            assert!(result.is_err());
+        }
 
-    #[test]
-    fn thousands_trailing() {
-        let result1 = quantity().then_ignore(end()).parse("12,345,678.");
-        let result2 = quantity().then_ignore(end()).parse("12.345.678,");
-        assert_eq!(result1, result2);
-        assert!(result2.is_ok());
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 12_345_678,
-                places: 0,
-            })
-        );
-    }
+        #[test]
+        fn decimals() {
+            let result1 = decimal_quantity().then_ignore(end()).parse("1.23");
+            let result2 = decimal_quantity().then_ignore(end()).parse("1,23");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 123,
+                    places: 2,
+                })
+            );
+        }
 
-    #[test]
-    fn thousands_invalid() {
-        let result = quantity().then_ignore(end()).parse("12.34.678");
-        assert!(result.is_err());
-    }
+        #[test]
+        fn decimals_like_thousands() {
+            let result1 = decimal_quantity().then_ignore(end()).parse("1.234");
+            let result2 = decimal_quantity().then_ignore(end()).parse("1,234");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 1234,
+                    places: 3,
+                })
+            );
+        }
 
-    #[test]
-    fn thousands() {
-        let result1 = quantity().then_ignore(end()).parse("12,345,678");
-        let result2 = quantity().then_ignore(end()).parse("12.345.678");
-        assert_eq!(result1, result2);
-        assert!(result2.is_ok());
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 12_345_678,
-                places: 0,
-            })
-        );
-    }
+        #[test]
+        fn thousands_trailing() {
+            let result1 = decimal_quantity().then_ignore(end()).parse("12,345,678.");
+            let result2 = decimal_quantity().then_ignore(end()).parse("12.345.678,");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 12_345_678,
+                    places: 0,
+                })
+            );
+        }
 
-    #[test]
-    fn thousands_and_decimals() {
-        let result1 = quantity().then_ignore(end()).parse("12,345.678");
-        let result2 = quantity().then_ignore(end()).parse("12.345,678");
-        assert_eq!(result1, result2);
-        assert_eq!(
-            result2,
-            Ok(Quantity {
-                mantissa: 12_345_678,
-                places: 3,
-            })
-        );
+        #[test]
+        fn thousands_invalid() {
+            let result = decimal_quantity().then_ignore(end()).parse("12.34.678");
+            assert!(result.is_err());
+        }
+
+        #[test]
+        fn thousands() {
+            let result1 = decimal_quantity().then_ignore(end()).parse("12,345,678");
+            let result2 = decimal_quantity().then_ignore(end()).parse("12.345.678");
+            assert!(result1.is_err());
+            assert!(result2.is_err());
+        }
+
+        #[test]
+        fn thousands_and_decimals() {
+            let result1 = decimal_quantity().then_ignore(end()).parse("12,345.678");
+            let result2 = decimal_quantity().then_ignore(end()).parse("12.345,678");
+            assert_eq!(result1, result2);
+            assert_eq!(
+                result2,
+                Ok(Quantity {
+                    mantissa: 12_345_678,
+                    places: 3,
+                })
+            );
+        }
     }
 }
