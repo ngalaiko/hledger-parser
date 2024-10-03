1ae6c9c0252de1006362ec13cbd34adefc4effef
Nikita Galaiko <nikita@galaiko.rocks>
2024-10-03 18:44:09 +0200

    parse date

--- src/component.rs
+++ src/component.rs
@@ -2,5 +2,6 @@ pub mod account_name;
 pub mod amount;
 pub mod comment;
 pub mod commodity;
+pub mod date;
 pub mod format;
 pub mod quantity;
---
+++ src/component/date.rs
@@ -0,0 +1,133 @@
+use chumsky::{
+    error::Simple,
+    prelude::{filter, just},
+    Parser,
+};
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct Date {
+    year: Option<u16>,
+    month: u16,
+    day: u16,
+}
+
+pub fn simple() -> impl Parser<char, Date, Error = Simple<char>> {
+    let digit = filter(move |c: &char| c.is_ascii_digit());
+    let year = digit
+        .repeated()
+        .exactly(4)
+        .collect::<String>()
+        .map(|m| m.parse::<u16>().unwrap());
+    let month = digit
+        .repeated()
+        .at_least(1)
+        .at_most(2)
+        .collect::<String>()
+        .map(|m| m.parse::<u16>().unwrap())
+        .validate(|s, span, emit| {
+            if !(1..=12).contains(&s) {
+                emit(Simple::custom(
+                    span,
+                    format!("{s} must be between 1 and 12."),
+                ));
+            }
+            s
+        });
+    let day = digit
+        .repeated()
+        .at_least(1)
+        .at_most(2)
+        .collect::<String>()
+        .map(|m| m.parse::<u16>().unwrap())
+        .validate(|s, span, emit| {
+            if !(1..=31).contains(&s) {
+                emit(Simple::custom(
+                    span,
+                    format!("{s} must be between 1 and 31."),
+                ));
+            }
+            s
+        });
+    let with_year = |separator: char| {
+        year.then_ignore(just(separator))
+            .then(month)
+            .then_ignore(just(separator))
+            .then(day)
+            .map(|((year, month), day)| Date {
+                year: Some(year),
+                month,
+                day,
+            })
+    };
+    let with_year = with_year('/').or(with_year('.')).or(with_year('-'));
+    let without_year = |separator: char| {
+        month
+            .then_ignore(just(separator))
+            .then(day)
+            .map(|(month, day)| Date {
+                year: None,
+                month,
+                day,
+            })
+    };
+    let without_year = without_year('/')
+        .or(without_year('.'))
+        .or(without_year('-'));
+    with_year.or(without_year)
+}
+
+#[cfg(test)]
+mod tests {
+    use chumsky::prelude::end;
+
+    use super::*;
+
+    #[test]
+    fn simple_date() {
+        for (input, expected) in [
+            (
+                "2010-01-31",
+                Date {
+                    year: Some(2010),
+                    month: 1,
+                    day: 31,
+                },
+            ),
+            (
+                "2010-01-31",
+                Date {
+                    year: Some(2010),
+                    month: 1,
+                    day: 31,
+                },
+            ),
+            (
+                "2010/01/31",
+                Date {
+                    year: Some(2010),
+                    month: 1,
+                    day: 31,
+                },
+            ),
+            (
+                "01/31",
+                Date {
+                    year: None,
+                    month: 1,
+                    day: 31,
+                },
+            ),
+            (
+                "1-31",
+                Date {
+                    year: None,
+                    month: 1,
+                    day: 31,
+                },
+            ),
+        ] {
+            let result = simple().then_ignore(end()).parse(input);
+            assert_eq!(result, Ok(expected), "{input}");
+        }
+    }
+}
